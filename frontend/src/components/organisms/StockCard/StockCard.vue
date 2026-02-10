8d92316dfd5b0eff268011ec229b96ec7097f6e6
Mando75317 <bcraadsveld@gmail.com>
2026-02-10 13:48:32 +0100

    oke

--- frontend/src/components/organisms/StockCard/StockCard.vue
+++ frontend/src/components/organisms/StockCard/StockCard.vue
@@ -1,17 +1,27 @@
 <template>
-    <div
-      v-for="stock in stocks"
-      :key="stock.name"
-      class="bg-white rounded-lg shadow-md p-4 border border-gray-200 hover:shadow-lg transition-shadow"
-    >
+    <div class="bg-white rounded-lg shadow-md p-4 border border-gray-200 hover:shadow-lg transition-shadow">
       <div class="flex justify-between items-center">
         <StockInfo :stock="stock" />
         <StockPrice :stock="stock" />
       </div>
+      <button type="button" >Buy</button>
     </div>
 </template>
 
 <script setup>
-    import StockInfo from '../../molecules/StockInfo/StockInfo.vue'
-    import StockPrice from '../../molecules/StockPrice/StockPrice.vue'
+import StockInfo from '../../molecules/StockInfo/StockInfo.vue'
+import StockPrice from '../../molecules/StockPrice/StockPrice.vue'
+
+const emits = defineEmits(['buy', 'sell'])
+
+const props = defineProps({
+  stock: {
+    type: Object, 
+    required: true
+  }
+})
+
+
+
+
 </script>
--- frontend/src/components/organisms/StockList/StockList.vue
+++ frontend/src/components/organisms/StockList/StockList.vue
@@ -1,30 +1,12 @@
 <template>
   <div class="grid gap-4" :class="gridClasses">
-    
-    <!-- <StockCard /> -->
-    
-    <div
-      v-for="stock in stocks"
-      :key="stock.name"
-      class="bg-white rounded-lg shadow-md p-4 border border-gray-200 hover:shadow-lg transition-shadow"
-    >
-    
-      <div class="flex justify-between items-center">
-        <StockInfo :stock="stock" />
-        <StockPrice :stock="stock" />
-      </div>
-
-      
-
-    </div>
+    <StockCard v-for="stock in stocks" :key="stock.name" :stock="stock" @buy=""></StockCard>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-import StockInfo from '../../molecules/StockInfo/StockInfo.vue'
-import StockPrice from '../../molecules/StockPrice/StockPrice.vue'
-import StockCard from '../../organisms/StockCard/StockCard.vue'
+import StockCard from '../StockCard/StockCard.vue'
 
 const props = defineProps({
   stocks: {
